877e5cf5dda9cc63003baac81115b4c5149c1106
josdic1 <joshuadicker@gmail.com>
2025-08-05 10:18:10 -0400

    adding methods to Book class - find_by_reader_id

--- lib/borrowing.py
+++ lib/borrowing.py
@@ -14,5 +14,47 @@ class Borrowing:
     def __repr__(self):
         return f"Borrowing | reader_id: {self.reader.id} book_id: {self.book.id}"
     
+    @property
+    def reader_id(self):
+        return self._reader_id
+    
+    @reader_id.setter
+    def reader_id(self, value):
+        if isinstance (value, int) and value > 0:
+            self._reader_id = value
+        else:
+            raise ValueError("Input is missing or not an integer")
+    
+    @property
+    def book_id(self):
+        return self._book_id
+    
+    @book_id.setter
+    def book_id(self, value):
+        if isinstance (value, int) and value > 0:
+            self._book_id = value
+        else:
+            raise ValueError("Input is missing or not an integer")
+    
+    @classmethod
+    def find_by_reader_id(cls, reader_id):
+        CURSOR.execute("SELECT * FROM borrowings WHERE reader_id =?", (reader_id,))
+        rows = CURSOR.fetchall()
+        return rows if rows else []
+    
+    @classmethod
+    def find_by_book_id(cls, book_id):
+        CURSOR.execute("SELECT * FROM boorrowings WHERE book_id = ?", (book_id,))
+        rows = CURSOR.fetchall()
+        return rows if rows else []
+        
+    @classmethod
+    def add_new(cls, reader_id, book_id):
+        CURSOR.execute("INSERT INTO borrowings (reader_id, book_id) VALUES (?,?)", (reader_id, book_id,))
+        CONN.commit()
 
     
+
+        
+    
+
